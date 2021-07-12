e70c574ca7e71d8d86d7d1122fc5a9667fcffdd3
sunakshiwhjr <77574525+sunakshiwhjr@users.noreply.github.com>
2021-07-12 12:58:18 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,80 +1,60 @@
-var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
+var trex, trex_running;
+var edges;
+var ground, groundImg;
 
+function preload()
+{
 
-
-
-var score;
-
-
-function preload(){
-  trex_running = loadAnimation("trex1.png","trex2.png","trex3.png");
-  trex_collided = loadImage("trex_collided.png");
-  
-  groundImage = loadImage("ground2.png");
-  
- 
-  
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  groundImg = loadImage("ground2.png");
 }
 
-function setup() {
-
-  createCanvas(600,200)
+function setup(){
+  createCanvas(600,200);
   
   //create a trex sprite
-  trex = createSprite(50,160,20,50);
-  trex.addAnimation("running", trex_running);
-  trex.scale = 0.5;
-  
-  //create a ground sprite
-  ground = createSprite(200,180,400,20);
-  ground.addImage("ground",groundImage);
-  ground.x = ground.width /2;
-  ground.velocityX = -4;
-  
-  //creating invisible ground
-  invisibleGround = createSprite(200,190,400,10);
-  invisibleGround.visible = false;
-  
-  //generate random numbers
-  var rand =  Math.round(random(1,100))
-  console.log(rand)
+  trex = createSprite(144, 120, 10, 30);
+  trex.addAnimation("t", trex_running);
+  trex.scale = 0.5
+ // trex.velocityX = 2;
+ 
 
+  //create the ground
+  ground = createSprite(300, 170, 600, 20);
+  ground.addImage(groundImg);
+
+  edges = createEdgeSprites();
 }
 
-function draw() {
-  //set background color
-  background(180);
-  
-  console.log(trex.y)
-  
-  
-  
-  // jump when the space key is pressed
-  if(keyDown("space")&& trex.y >= 100) {
+function draw(){
+  background(190);
+
+  textSize(20);
+  fill("White");
+  text(mouseX + "," + mouseY, mouseX, mouseY);
+
+//make ground move
+ground.velocityX = -2;
+
+//reset the ground
+if(ground.x < 0)
+{
+    ground.x = 300
+}
+
+console.log(ground.x);
+  //make trex jump
+
+  if(keyDown("space"))
+  {
     trex.velocityY = -10;
   }
-  
-  trex.velocityY = trex.velocityY + 0.8
-  
-  if (ground.x < 0){
-    ground.x = ground.width/2;
-  }
-  
-  //stop trex from falling down
-  trex.collide(invisibleGround);
-  
-  //Spawn Clouds
-  spawnClouds()
-  
+
+  trex.velocityY = trex.velocityY + 0.5;
+
+   trex.collide(ground)
+   trex.collide(edges[3]);
+
   drawSprites();
+
 }
-
-//function to spawn the clouds
-function spawnClouds(){
- // write your code here 
- 
-}
-
-
-
